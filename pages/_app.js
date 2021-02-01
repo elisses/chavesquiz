6adfd02696818764666f64101e34c8a5e8c08b2a
Elis <fernandalisses@gmail.com>
2021-02-01 19:59:21 +0100

    aula3 e aula2

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/prop-types */
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 import db from '../db.json';
+
 const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
@@ -20,18 +25,21 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-
+      <Head>
+        <title>Turma do Chaves</title>
+        <link rel="icon" href="https://apprecs.org/gp/images/app-icons/300/08/br.com.blooti.chavinhogroove.jpg" type="image/gif" sizes="16x16" />
+      </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
-import styled from 'styled-components'
+/* eslint-disable react/react-in-jsx-scope */
+import styled from 'styled-components';
 
 const FooterWrapper = styled.footer`
   background-color: #00000070;
@@ -25,21 +26,21 @@ const FooterWrapper = styled.footer`
 `;
 
 export default function Footer(props) {
-    return (
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        <FooterWrapper {...props}>
-            <a href="https://www.alura.com.br/">
-                <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-            </a>
-            <p>
-                Orgulhosamente criado durante
-                {' '}
-                a
-                {' '}
-                <a href="https://www.alura.com.br/">
-                    <span>Imersão React da Alura</span>
-                </a>
-            </p>
-        </FooterWrapper>
-    );
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <FooterWrapper {...props}>
+      <a href="https://www.alura.com.br/">
+        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      </a>
+      <p>
+        Orgulhosamente criado durante
+        {' '}
+        a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          <span>Imersão React da Alura</span>
+        </a>
+      </p>
+    </FooterWrapper>
+  );
 }
